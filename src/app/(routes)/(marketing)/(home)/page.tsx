d5faa1872d1f74a4ac1f94ede3081cee74b7c1bc
lakshyaCreates <lakshya.creates07@gmail.com>
2024-10-23 15:50:27 +0530

    Fix merge

--- src/app/(routes)/(marketing)/(home)/page.tsx
+++ src/app/(routes)/(marketing)/(home)/page.tsx
@@ -3,11 +3,7 @@ import { CTABanner } from "./_components/cta-banner";
 import { Hero } from "./_components/hero";
 import { Philosophy } from "./_components/philosophy";
 import { RecentEvents } from "./_components/recent-events";
-<<<<<<< HEAD
 import { RecentProjects } from "./_components/recent-projects";
-=======
->>>>>>> d32fb54bcfe1da14248896bf89f7ca7d22a7d75d
-import { TopNotificationBar } from "./_components/top-notification-bar";
 
 export default function HomePage() {
     return (
@@ -16,11 +12,8 @@ export default function HomePage() {
             <Philosophy />
             <CSRPartners />
             <RecentEvents />
-<<<<<<< HEAD
             <CTABanner />
             <RecentProjects />
-=======
->>>>>>> d32fb54bcfe1da14248896bf89f7ca7d22a7d75d
         </div>
     );
 }
