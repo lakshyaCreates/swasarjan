da0196fb2ac519eef2884e18a3cbd7ea38960ed4
lakshyaCreates <lakshya.creates07@gmail.com>
2024-10-29 11:58:56 +0530

    fix typo: in cta-banner

--- src/app/(routes)/(marketing)/_components/cta-banner.tsx
+++ src/app/(routes)/(marketing)/_components/cta-banner.tsx
@@ -9,7 +9,7 @@ export const CTABanner = () => {
     return (
         <div className="h-fit w-screen bg-brand-orange-500 md:h-40">
             <Wrapper>
-            <div className="mdm:py-0 flex h-full flex-col items-center justify-center gap-y-6 py-6 md:flex-row md:gap-y-0">
+                <div className="flex h-full flex-col items-center justify-center gap-y-6 py-6 md:flex-row md:gap-y-0 md:py-0">
                     <div className="space-y-2 px-4 md:px-0">
                         <SectionHeading className="w-full text-center !leading-7 text-white md:text-start">
                             Looking to contribute to a cause you feel close to?
